0dd95b33042a223ad8ed71f0e9a46bab21407b10
Tomash <tomasz9201@gmail.com>
2019-02-02 16:18:59 +0200

    up

---
+++ build-node.js
@@ -0,0 +1,49 @@
+process.env.NODE_ENV = 'production';
+
+const webpack = require('webpack');
+const config = require('./server.webpack');
+const compiler = webpack(config);
+const chalk = require('chalk');
+
+console.log(
+	`${chalk.magenta(
+		'[INFO]'
+	)} Before running SSR production build you have to run ${chalk.underline(
+		'npm run build'
+	)} to create an optimized production build of your ${chalk.underline(
+		'create-react-app'
+	)} application.`
+);
+
+console.log(chalk.cyan('\nCreating production Server build...\n'));
+
+
+compiler.run((err, stats) => {
+	if (err) {
+		console.error(err.stack || err);
+		if (err.details) {
+			console.error(err.details);
+		}
+		return;
+	}
+
+	const info = stats.toJson();
+
+	if (stats.hasErrors()) {
+		console.error(info.errors);
+	}
+
+	if (stats.hasWarnings()) {
+		console.warn(info.warnings);
+	}
+	const statsString = stats.toString({
+		colors: true,
+	});
+	console.log(statsString);
+
+	if (stats.hasErrors()) {
+		console.error(chalk.red('\nBuild failed!'));
+	} else {
+		console.log(chalk.green('\nBuild successful!'));
+	}
+});
---
+++ server.webpack.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const config = {
+	mode: NODE_ENV,
+	target: 'node',
+	entry: ['./server.js'],
+	output: {
+		path: path.join(__dirname, './build'),
+		// Build it as a commonjs library so we can include it
+		filename: 'server.js',
+		// Build it as a commonjs library so we can include it
+		libraryTarget: 'commonjs',
+	},
+	externals: [],
+	module: {
+		rules: [
+			{
+				test: /\.(jpe?g|png|gif|svg|ico)$/i,
+				loader: 'file-loader',
+				options: {
+					limit: 10000,
+					name: '[name].[hash:8].[ext]',
+					publicPath: '/static/media/',
+					outputPath: '../client/static/media/',
+				},
+			},
+			{
+				test: /\.js$/,
+				exclude: [/[/\\\\]node_modules[/\\\\]/],
+				use: [
+					{
+						loader: require.resolve('babel-loader'),
+					},
+				],
+			},
+			{ test: /\.(.css|scss)$/, loader: 'ignore-loader' },
+		],
+	},
+	plugins: [
+		new webpack.NamedModulesPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
+	],
+};
+
+if (NODE_ENV === 'production') {
+	config.externals.unshift(nodeExternals())
+	config.plugins.push(
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		new webpack.optimize.OccurrenceOrderPlugin(),
+	)
+} else {
+	config.entry.unshift('webpack/hot/poll?1000'); 
+	config.externals.unshift(nodeExternals({
+		// Include the hot reload polling code in the bundle though
+		whitelist: ['webpack/hot/poll?1000'],
+	}))
+	config.plugins.unshift(
+		new webpack.HotModuleReplacementPlugin(),
+	)
+	
+}
+
+
+module.exports = config
