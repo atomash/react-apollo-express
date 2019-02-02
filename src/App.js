c17503be16985c08cdfc193dd830468750bbef70
Tomash <tomasz9201@gmail.com>
2019-02-02 16:17:47 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import Layout from './Layout';
+import AllUsers from './AllUsers';
+import TopPosts from './TopPosts';
+import NewPost from './NewPost';
+import Post from './Post';
+import AppRouter from './AppRouter';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = (props) => (
+  <ApolloProvider client={props.client}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+  </ApolloProvider>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: ApolloLink.from([
+     onError(({ graphQLErrors, networkError }) => {
+       if (graphQLErrors)
+         graphQLErrors.map(({ message, locations, path }) =>
+           console.log(
+             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+           ),
+         );
+       if (networkError) console.log(`[Network error]: ${networkError}`);
+     }),
+     new HttpLink({
+       uri: 'https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex',
+       // credentials: 'same-origin'
+     })
+  ]),
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+hydrate(<App client={client} />, document.getElementById('app'));
